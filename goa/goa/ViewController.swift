13a3742deba3b6f5478f0e1c6b3cb300cac6563d
Evann-Li <50848953+Evann-Li@users.noreply.github.com>
2019-05-20 15:13:15 -0700

    Add files via upload

---
+++ goa/goa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goa
+//
+//  Created by Evann Li on 2019-05-18.
+//  Copyright © 2019 Evann Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
